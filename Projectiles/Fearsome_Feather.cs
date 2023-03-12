27c1c6741b13c6c7b46d8fb0e79447b3a4f0acf9
Zavarski <hrom0405@gmail.com>
2023-03-12 11:32:00 +0800

    Anti-Plagiarism

--- Projectiles/Fearsome_Feather.cs
+++ Projectiles/Fearsome_Feather.cs
@@ -37,33 +37,17 @@ namespace SoulsBetterDLC.Projectiles
         public override void Kill(int timeLeft)
         {
             SoundEngine.PlaySound(in SoundID.Item14, Projectile.position);
-            Projectile.position.X = Projectile.position.X + (Projectile.width / 2);
-            Projectile.position.Y = Projectile.position.Y + (Projectile.height / 2);
-            Projectile.width = 50;
-            Projectile.height = 50;
-            Projectile.position.X = Projectile.position.X - (Projectile.width / 2);
-            Projectile.position.Y = Projectile.position.Y - (Projectile.height / 2);
             for (int i = 0; i < 15; i++)
             {
-                int exzploDust = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.UnusedWhiteBluePurple, 0f, 0f, 100, default(Color), 1.2f);
-                Dust obj = Main.dust[exzploDust];
-                obj.velocity *= 3f;
+                int exzploDust = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.UnusedWhiteBluePurple, 0f, 0f, 100, default, 1.2f);
+                Dust dustLmao = Main.dust[exzploDust];
+                dustLmao.velocity *= 3f;
                 if (Main.rand.NextBool(2))
                 {
                     Main.dust[exzploDust].scale = 0.5f;
                     Main.dust[exzploDust].fadeIn = 1f + Main.rand.Next(10) * 0.1f;
                 }
             }
-            /*for (int j = 0; j < 30; j++)
-            {
-                int num2 = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.UnusedWhiteBluePurple, 0f, 0f, 100, default(Color), 1.7f);
-                Main.dust[num2].noGravity = true;
-                Dust obj2 = Main.dust[num2];
-                obj2.velocity *= 5f;
-                num2 = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.UnusedWhiteBluePurple, 0f, 0f, 100);
-                Dust obj3 = Main.dust[num2];
-                obj3.velocity *= 2f;
-            }*/
         }
     }
 }
